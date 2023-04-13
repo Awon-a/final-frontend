e293bba6acc606298929781765806a48b3a5ee58
Tsupikov Vladislav <vl.tsupikov@gmail.com>
2023-04-13 10:06:01 +0600

    feat(academic-plan): added list disciplines blocks and tabs disciplines blocks, scrolling for dragging

--- src/components/CreatePlan/DnDDisciplines.tsx
+++ src/components/CreatePlan/DnDDisciplines.tsx
@@ -1,10 +1,62 @@
-import { useDrag } from "react-dnd";
+import { useRef } from "react";
+import { useDrag, useDragLayer } from "react-dnd";
+import * as Scroller from "react-scroll";
 
 export const DisciplineTypes = {
   Discipline: "Discipline",
 };
 
 function Discipline({ disciplines, handleDisciplineMove }: any) {
+  // let scrollTimeout: any;
+  // let scrollVelocity = 0;
+
+  // window.addEventListener("dragover", (e) => {
+  //   e.preventDefault();
+  //   const { clientY } = e;
+  //   const { innerHeight } = window;
+  //   const scrollThreshold = 100;
+  //   if (clientY < scrollThreshold) {
+  //     scrollVelocity = -10;
+  //   } else if (clientY > innerHeight - scrollThreshold) {
+  //     scrollVelocity = 10;
+  //   } else {
+  //     scrollVelocity = 0;
+  //     cancelAnimationFrame(scrollTimeout);
+  //     return;
+  //   }
+  //   if (!scrollTimeout) {
+  //     scrollTimeout = requestAnimationFrame(scrollPage);
+  //   }
+  // });
+
+  // function scrollPage() {
+  //   window.scrollBy(0, scrollVelocity);
+  //   scrollVelocity *= 0.9;
+  //   if (Math.abs(scrollVelocity) < 1) {
+  //     scrollVelocity = 0;
+  //     cancelAnimationFrame(scrollTimeout);
+  //     scrollTimeout = null;
+  //     return;
+  //   }
+  //   scrollTimeout = requestAnimationFrame(scrollPage);
+  // }
+  // document.body.addEventListener("dragover", function (event) {
+  //   event.preventDefault(); // Отменяем действие по умолчанию браузера
+  //   var mouseY = event.clientY; // Получаем вертикальную позицию курсора
+  //   var offset = 250; // Задаем смещение от края страницы
+  //   var threshold = window.innerHeight - offset; // Вычисляем порог для прокрутки
+
+  //   if (mouseY < offset) {
+  //     // Если курсор находится в верхней области
+  //     window.scrollBy(0, -150); // Прокручиваем страницу вверх
+  //   } else if (mouseY > threshold) {
+  //     // Если курсор находится в нижней области
+  //     window.scrollBy(0, 150); // Прокручиваем страницу вниз
+  //   }
+  // });
+
+  const scrollRef: any = useRef(null);
+
   return (
     <div className="table-container">
       <div className="disicplines-table-caption">Дисциплины</div>
@@ -31,20 +83,48 @@ function Discipline({ disciplines, handleDisciplineMove }: any) {
   );
 }
 
-const TableRow = ({ discipline, handleDisciplineMove }: any) => {
+export function CustomDragLayer() {
+  const { isDragging, item, currentOffset } = useDragLayer((monitor) => ({
+    item: monitor.getItem(),
+    currentOffset: monitor.getSourceClientOffset(),
+    isDragging: monitor.isDragging(),
+  }));
+
+  if (!isDragging) {
+    return null;
+  }
+
+  const { id } = item;
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        left: currentOffset!.x,
+        top: currentOffset!.y,
+        opacity: 1,
+      }}
+    >
+      <TableRow key={item.discipline.id} discipline={item.discipline} />
+    </div>
+  );
+}
+
+const TableRow = ({ discipline, handleDisciplineMove, scrollRef }: any) => {
   const [{ opacity }, dragRef] = useDrag(() => ({
     type: DisciplineTypes.Discipline,
     isDragging: (monitor) => !!monitor.isDragging,
     item: { discipline },
     collect: (monitor) => ({
       opacity: monitor.isDragging() ? 0.5 : 1,
+      connectDragPreview: monitor.canDrag(),
     }),
   }));
 
   return (
     <tr
       ref={dragRef}
-      style={{ opacity: opacity }}
+      style={{ opacity: opacity, cursor: "move" }}
       className="discipline-table-tr"
       key={discipline.id}
       onMouseDown={() => {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,25 +11,26 @@ export interface HeaderProps {
 const Header = ({ currentPath }: HeaderProps) => {
   const userString = localStorage.getItem("user");
   const user = userString && JSON.parse(userString);
-  console.log({ user: user.email });
 
   return (
     <>
-      <div className="header-line">
-        <AvatarMenu userId={user?.id} />
-      </div>
-      <div className="menu">
-        <Menu mode="horizontal" selectedKeys={[currentPath]}>
-          <Menu.Item key={Paths.AcademicPlans}>
-            <Link to={Paths.AcademicPlans}>Учебные планы</Link>
-          </Menu.Item>
-          <Menu.Item key={Paths.Disciplines}>
-            <Link to={Paths.Disciplines}>Дисциплины</Link>
-          </Menu.Item>
-          <Menu.Item key={Paths.Competencies}>
-            <Link to={Paths.Competencies}>Компетенции</Link>
-          </Menu.Item>
-        </Menu>
+      <div className="header-container">
+        <div className="header-line">
+          <AvatarMenu userId={user?.id} />
+        </div>
+        <div className="menu">
+          <Menu mode="horizontal" selectedKeys={[currentPath]}>
+            <Menu.Item key={Paths.AcademicPlans}>
+              <Link to={Paths.AcademicPlans}>Учебные планы</Link>
+            </Menu.Item>
+            <Menu.Item key={Paths.Disciplines}>
+              <Link to={Paths.Disciplines}>Дисциплины</Link>
+            </Menu.Item>
+            <Menu.Item key={Paths.Competencies}>
+              <Link to={Paths.Competencies}>Компетенции</Link>
+            </Menu.Item>
+          </Menu>
+        </div>
       </div>
     </>
   );
